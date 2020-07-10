5b99c75e9650134a9254a40987ec753d6ba3ff92
THIBAULT <jerome.thibault@atos.net>
2020-07-10 13:30:35 +0200

    first commit

---
+++ book-flask-app/app/app.py
@@ -0,0 +1,30 @@
+from flask import (
+    Flask,jsonify,request
+)
+from db.book import (
+    findAll, save, delete, update
+)
+
+app = Flask(__name__)
+       
+@app.route('/book', methods=['GET'])
+def getBooks():
+    # TODO
+    return
+
+@app.route('/book', methods=['POST'])
+def saveBook():
+    # TODO
+    return
+
+@app.route('/book', methods=['DELETE'])
+def deleteBook():
+    # TODO
+    return
+
+@app.route('/book', methods=['PUT'])
+def updateBook():
+    # TODO
+    return
+    
+app.run()
---
+++ book-flask-app/app/db/book.py
@@ -0,0 +1,35 @@
+import sqlite3
+import os
+
+if os.path.exists('book.db'):
+    os.remove('book.db')
+
+conn = sqlite3.connect('book.db')
+
+conn.execute("CREATE TABLE book (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, author TEXT NOT NULL)")
+conn.execute("INSERT INTO book (name,author) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald')")
+conn.execute("INSERT INTO book (name,author) VALUES ('Moby Dick', 'Herman Melville')")
+conn.commit()
+conn.close()
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def findAll():
+    # TODO
+    return
+
+def save(name,author):
+    # TODO
+    return
+
+def delete(id):
+    # TODO
+    return
+
+def update(id,name,author):
+    # TODO
+    return
